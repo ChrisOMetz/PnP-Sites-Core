0f79bdae01af98177700fd9f10fb494d58c52e3f
Christian Metz <christian@cloudwerk.com>
2020-05-29 11:40:14 +0200

    Init fork

--- Core/OfficeDevPnP.Core/Utilities/Context/ClientContextType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OfficeDevPnP.Core.Utilities.Context
-{
-    internal enum ClientContextType
-    {
-        SharePointACSAppOnly = 0,
-        AzureADCredentials = 1,
-        AzureADCertificate = 2
-    }
-}
